5410f638493e03c88bd5f47758082ce999a9966d
Sparue <admin@sparue.com>
2021-08-18 03:04:37 +0530

    first

---
+++ contact.php
@@ -0,0 +1,454 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="utf-8">
+        <meta content="width=device-width, initial-scale=1.0" name="viewport">
+        <title>OnlineTechs365</title>
+    
+        <link href="./assets/img/logo.AVIF" rel="icon">
+    
+        <link
+            href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Roboto:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
+            rel="stylesheet">
+    
+        <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+        <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+            <link href="assets/vendor/icofont/icofont.min.css" rel="stylesheet">
+
+        <link href="assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+        <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+    
+        <link href="assets/css/style1.css" rel="stylesheet">
+    </head>
+    
+<header id="header" style="background:linear-gradient(to bottom,#e7d6c6ba, #1e1e1fcc),  url(./assets/img/contactbg1.AVIF)">
+<nav class="navbar fixed-top navbar-expand navbar-light bg-light">
+        <div class="container d-flex align-items-center navbar-start">
+            <a href="index.html" class="logo mr-auto"><img src="./assets/img/logonbg.AVIF"></a>
+
+            <div >
+                <ul class=" nav-menu d-none d-lg-block">
+                    <li ><a href="index.html">Home</a></li>
+                    <li ><a href="about.html">About Us</a></li>
+                    <li><a href="service.html">Services</a></li>
+                    <li><a href="pricing.html">Pricing</a></li>
+                    <li class="active"><a href="contact.php">Contact Us</a></li>
+                    <li class="ml-5 text-right">
+                        <a href="contact.php"> <i class="bx bx-phone"></i> <b>USA:</b> +1-888-5131305</a>
+                          
+                        <a href="contact.php"> <i class="bx bx-phone"></i> <b>Uk:</b> +44 800-102-6324</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+ 
+
+    <div class="container mt-5 pt-5" data-aos="fade-up">
+        <div class="row">
+            <div class="col-lg-6 col-md-6">
+                <div class="banner-text-wrap ">
+                    <h1>Contact us</h1>  
+                </div>
+                 <div class="breadcrumb-top breadcrumb-contact">
+                        <ol class="breadcrumb" >
+                            <!-- <li class="breadcrumb-item " ><a href="index.html">Home </a></li> -->
+                            <li class="breadcrumb-item active"style=" margin:22px -40px"><p style="color:white">Home</p>&nbsp;/ContactUs</li>
+                        </ol>
+                </div> 
+               
+            </div>
+            <div class="col-lg-6 col-md-6 text-right btn-all contact-btn">
+                <button type="button" class="btn des-btn-top" data-toggle="modal" data-target="#exampleModalLong">
+                    *Disclaimer
+                </button>
+            </div>
+         </div>
+    </div>
+  
+</header>
+<div class="modal fade " id="exampleModalLong" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+    
+        <div class="modal-body p-4">
+            <p  class="text-justify des">
+                 <strong>"OnlineTechs365 (Owned by VRM PITTA LLC)"</strong>
+                provides services for diagnostic and technical support issues for desktops, laptops, mobile devices,
+                and tablets. We are a <b style="color:orangered">THIRD PARTY</b> solution and we are <span style="color:orangered"><b>"NOT AFFILIATED"</b></span> with any brands, trademarks,
+                logos, or any other company names used. The brand names, trademarks, and logos belong to their
+                respective owners and are for representation purposes only.
+            </p>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn des-btn-top" data-dismiss="modal">Close</button>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+
+
+  <section id="about-us" class="about-us section-bg">
+    <div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+            <h2>We are here to</h2>
+        </div>
+        <div class="row content text-center">
+            <div class="col-lg-12 pt-4 pt-lg-0" data-aos="fade-up">
+                <h3>Feel free to contact us</h3>
+                <p class="text-justify">
+                    At OnlineTechs365, our skilled in house technicians are committed to making sure you get the service
+                    you need, the way you need it. Fill out the form below and we'll contact you within 30 minutes (during regular business hours). Or give us a call, we're always happy to help!</p>
+ 
+            </div>
+        </div>
+    </div>
+</section>
+
+
+
+<section id="about-wrap-top" class="about-wrap-top contact-us-page">
+    <div class="container" data-aos="zoom-in">
+        <div class="row content ">
+         
+            <div class="col-lg-5 md-6 text-left address-wrap p-5">
+                <!-- <h3><b><strong>OnlineTechs365 </strong>Headquarters:</b></h3>
+                <p class="pt-2"> 
+                    <h6>Address:</h6>
+                    <p><b>USA:</b> 
+                    
+                    71-75 Shelton Street  Covent Garden, 
+                      London, WC2H 9JQ </p>
+                    <p> <i class="bx bx-phone"></i>   +1-888-5131305 <br> 
+                     </p>
+                       <p> <i class="bx bx-phone"></i> <b>UK:</b> +44 800-102-6324 </p>
+                    <p><b>Email:</b>support@onlinetechs365.com</p> 
+                    <p><b> Hours:</b>Mon-Friday 7am-9pm PST, Saturday 9am-3pm PST</p> 
+                </p>   -->
+                <div class="footer-text ">
+                <h4><strong>OnlineTechs365 </strong></h4>
+                    <p>
+                        <b>UK Office:</b> <br>
+                    <p class="pl-4">
+                        
+                        71-75 Shelton Street <br>
+                        Covent Garden, <br>
+                        London, WC2H 9JQ <br>
+                        <i class="bx bx-phone"></i> <b>Phone No:</b> +44 800-102-6324
+                    </p> <br>
+                    <b>USA Office:</b> <br>
+                    <p class="pl-4">
+                        
+                        30 N Gould St <br>
+                        STE R Sheridan <br>
+                        Wyoming 82801 <br>
+                        <i class="bx bx-phone"></i> <b>Phone No:</b> +1-888-5131305 <br> 
+                            
+                    </p>
+
+
+                    </p>
+                </div>
+
+            </div>
+            <div class="col-lg-7 md-8 text-justify">
+                <div class="map-data">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3330.58203436002!2d-111.939487!3d33.408067!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x872b08c6306aaaab%3A0xae6f13d0b5d235ff!2s64%20E%20Broadway%20Rd%20%23200%2C%20Tempe%2C%20AZ%2085282!5e0!3m2!1sen!2sus!4v1628486604410!5m2!1sen!2sus" width="100%" height="350px" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+                </div>
+        </div>
+        </div>
+    </div>
+</section>
+
+<section id="contact" class="contact section-bg clients">
+    <div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+            <h2>Follow <strong>OnlineTechs365 </strong>Online!</h2>
+        </div>
+        <div class="row content text-center">
+            <div class="col-lg-12 pt-4 pt-lg-0" data-aos="fade-up">
+                <h5>Stay up to date with news about the latest in technology, promos, job listings, and more.
+
+                </h5>
+                    <div class="contact-form pt-5">
+                        <form form action="" method="post" >
+
+                            <div class="row">
+                                <div class="col-sm-2 col-xs-2"> </div>
+      
+                                <div class="col-sm-4 col-xs-4">
+                                    <div class="form-group">
+                                        <input type="text" class="form-control" name="first_name" placeholder="First name">
+                                    </div>
+                                </div>
+                                <div class="col-sm-4 col-xs-4">
+                                    <div class="form-group">
+                                        <input type="text" class="form-control" name="last_name" placeholder="Last Name">
+                                    </div>
+                                </div>
+                                <div class="col-sm-2 col-xs-2"> </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-sm-2 col-xs-2"> </div>
+      
+                                <div class="col-sm-4 col-xs-4">
+                                    <div class="form-group">
+                                        <input type="text" class="form-control" name="phno" placeholder="Contact No">
+                                    </div>
+                                </div>
+                                <div class="col-sm-4 col-xs-4"> 
+                                    <div class="form-group">
+                                        <input type="email" class="form-control" name="email" placeholder="Email">
+                                    </div>
+                                </div>
+                                <div class="col-sm-2 col-xs-2"> </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-sm-2 col-xs-2"> </div>
+                                <div class="col-sm-8">
+                                    <div class="form-group">
+                                        <textarea class="form-control" name="message" rows="3"
+                                            placeholder="Message"></textarea>
+                                    </div>
+                                </div>
+                                <div class="col-sm-2 col-xs-2"> </div>
+                            </div>
+                            <div class="btn btn-wrap-form  mt-3">
+                            <input type="submit" name="submit"  class="btn helpbtn">
+                                <!--<button type="button" name="submit" class="btn helpbtn">Submit</button>-->
+                            </div>
+                        </form>
+                    </div>
+               
+            </div>
+        </div>
+    </div>
+</section>
+
+
+
+
+
+
+<section id="about-us" class="about-us ">
+    <div class="container" data-aos="fade-up">
+        <div class="row content text-center">
+        <div class="col-lg-12  pt-lg-0 " data-aos="fade-up">
+                <button type="button" class="btn sticky-btn helpbtn" data-toggle="modal" data-target="#exampleModalLong">
+                    *Disclaimer
+                </button>
+              
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<footer id="footer" class="footer pt-5">
+    <div class="container">
+        <div class="row ">
+            <div class="col-lg-3 col-md-3 text-left">
+                <div class="footer-heading ">
+                    <h5><u>Contact Us</u></h5>
+                </div>
+                <div class="footer-text ">
+                    <p>
+                    <p>
+                        <b>UK Office:</b> <br>
+                        71-75 Shelton Street <br>
+                        Covent Garden, <br>
+                        London WC2H 9JQ <br>
+                        <i class="bx bx-phone"></i> <b>Phone No:</b> +44 800-102-6324
+                    </p>
+                    <p>
+                        <b>USA Office:</b> <br>
+                        30 N Gould St <br>
+                        STE R Sheridan <br>
+                        Wyoming 82801 <br>
+                        <i class="bx bx-phone"></i> <b>Phone No:</b> +1-888-5131305 <br> 
+                            
+                    </p>
+                    <p>
+                        <b>E-mail:</b> Info@onlinetechs365.com <br>
+                    </p>
+
+
+                    </p>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-3 f-links">
+                <div class="footer-heading  text-left ml-5 ">
+                    <h6><u>Quick Links</u> </h6>
+                </div>
+                <div class="q-links ml-3  text-left">
+                    <ul>
+                        <li><a href="index.html">Home</a></li>
+                        <li><a href="about.html">About Us</a></li>
+                        <li><a href="service.html">Services</a></li>
+                        <li><a href="pricing.html">Pricing</a></li>
+                        <li><a href="contact.php">Contact Us</a></li>
+                    </ul>
+                </div>
+
+            </div>
+
+            <div class="col-lg-3 col-md-3 ">
+                <div class="f-heading ">
+                    <h6 id="bt-desc-btn"><u>*Disclaimer</u> </h6>
+                </div>
+          
+                <p class="text-justify" style="display:none;" id="bt-desc-text">
+                    <strong>"OnlineTechs365(owned by VRM PITTA LLC)" </strong>provides services for diagnostic and
+                    technical support issues for desktops, laptops, mobiles devices and tablets.
+                    We are a <b style="color:orangered">THIRD PARTY</b> solution and we are <b style="color:orangered">NOT AFFILIATED</b> with any brands, trademarks, logos, or any
+                    other company names used. The brand names, trademarks, and logos belong &nbsp to  their  respective owners
+                    and are &nbsp&nbsp&nbsp&nbsp&nbsp for &nbsp&nbsp&nbsp&nbsp&nbsp representation &nbsp&nbsp&nbsp&nbsp&nbsp purposes&nbsp&nbsp&nbsp&nbsp&nbsp only.
+                </p>
+              
+            </div>
+
+            <div class="col-lg-3 col-md-3 f-links" >
+                <div class="footer-heading text-left ml-5">
+                    <h6><u>Other Links</u> </h6>
+                </div>
+
+                <div class="q-links  text-left ml-2">
+                    <ul>
+                        <li><a href="privacy.html">Privacy Policy</a></li>
+                        <li><a href="terms.html">Terms and Conditions</a></li>
+                        <li><a href="sitemap.html">Sitemap</a></li>
+                        <li><a href="terms.html#refund-policy">Refund Policy</a></li>
+                        <li><a href="https://support.onlinetechs365.com/">IT Remote Service</a></li>
+                    </ul>
+                </div>
+                <div class="social-l text-left">
+                    <div class="f-heading mt-5">
+                        <h6><u>Social Links </u></h6>
+                    </div>
+                    <div class="social-links  mt-4  social-icons">
+                        <a href="https://twitter.com/VrmPittaLLC" class="twitter"><i class="bx bxl-twitter"></i></a>
+                        <a href="https://www.facebook.com/VRM-Pitta-685562348973444" class="facebook"><i class="bx bxl-facebook"></i></a>
+                        <a href="https://www.instagram.com/onlinetechs365/" class="instagram"><i
+                                class="bx bxl-instagram"></i></a>
+                        <a href="https://www.linkedin.com/company/vrm-pitta-llc/" class="linkedin"><i
+                                class="bx bxl-linkedin"></i></a>
+                    </div>
+                </div>
+                
+            </div>
+            <div class="col-lg-1 col-md-1">
+                <button onclick="topFunction()" class="back-to-top" id="myBtn" title="Go to top"><i class='bx bxs-up-arrow-alt'></i></button>
+                
+            </div>
+
+
+        </div>
+        <hr>
+        <div class="row copyright">
+            <div class="col-lg-12 col-md-12 text-center pb-2">
+                <div class="copyright-info">
+                    &copy; Copyright 2021 <a href="https://vrmpitta.com/"><b>VRM PITTA LLC</b></a> All Rights
+                    Reserved.
+
+                </div>
+            </div>
+        </div>
+</footer>
+ 
+ <?php 
+if(isset($_POST['submit'])){
+    $to = "support@onlinetechs365.com"; // this is your Email address
+    $from = $_POST['support@onlinetechs365.com']; // this is the sender's Email address
+    $first_name = $_POST['first_name'];
+    $last_name = $_POST['last_name'];
+    $phno = $_POST['phno'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+    
+    $subject = "Form submission";
+    $subject2 = "Copy of your form submission";
+    
+    
+
+    $message = $first_name . " " . $last_name . " wrote the following:" . "\n\n" . "Contact No:". $phno . "\n\n" . "Email:" . $email . "\n\n"  . "Message:" . $_POST['message'];
+    $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
+
+    $headers = "From:" . $from;
+    $headers2 = "From:" . $to;
+    mail($to,$subject,$message,$headers);
+    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
+    echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
+    // You can also use header('Location: thank_you.php'); to redirect to another page.
+    }
+?>
+
+ 
+  
+<body>
+
+
+
+
+    
+    
+    
+    
+<div class="fixed fixed--bottom cookie-disclaimer js-cookie-disclaimer">
+    <button id="close" style="background-color:black">Close ✖</button>
+    <div class="media__object media__object--right ">
+        <button class="button">Accept&nbsp;All</button>
+
+    </div>
+
+    <div class="media__body">
+        <p>We use cookies to understand how you use our site and to improve your experience, including personalizing
+            content and offering resources.
+            See our updated Privacy Policy and GDPR Policy to learn more. By continuing to use our site, you accept our
+            use of cookies.</p>
+    </div>
+
+</div>
+
+</div>
+
+<body>
+
+    <script>
+        const $$cookieDisclaimer = document.querySelector(".js-cookie-disclaimer");
+
+        if (!localStorage.getItem("cookieDisclaimer")) {
+            $$cookieDisclaimer.classList.add("is-active");
+        }
+
+        $$cookieDisclaimer.querySelector("button").addEventListener("click", () => {
+            localStorage.setItem("cookieDisclaimer", true);
+            $$cookieDisclaimer.classList.remove("is-active");
+        });
+    </script>
+
+
+
+
+
+
+
+    <script src="assets/vendor/jquery/jquery.min.js"></script>
+    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/vendor/jquery.easing/jquery.easing.min.js"></script>
+    <script src="assets/vendor/php-email-form/validate.js"></script>
+    <script src="assets/vendor/jquery-sticky/jquery.sticky.js"></script>
+    <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+    <script src="assets/vendor/venobox/venobox.min.js"></script>
+    <script src="assets/vendor/waypoints/jquery.waypoints.min.js"></script>
+    <script src="assets/vendor/owl.carousel/owl.carousel.min.js"></script>
+    <script src="assets/vendor/aos/aos.js"></script>
+
+    <script src="assets/js/main.js"></script>
+</body>
+
+</html>
